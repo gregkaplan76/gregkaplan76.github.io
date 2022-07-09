5ffd49fd052215885297b293cfd4898176644f56
gregkaplan76 <94259458+gregkaplan76@users.noreply.github.com>
2022-07-09 14:27:27 -0700

    howdy
    greeting

---
+++ greeting.js
@@ -0,0 +1,11 @@
+var username = prompt("What's your name?") 
+var limit = 20
+var greeting = 'Howdy ';
+/*var name = 'Molly';*/
+
+var welcomeMessage = greeting + username + ", thanks for visiting my page"+'!';
+
+var el = document.getElementById('greeting');
+
+el.textContent = welcomeMessage;
+
