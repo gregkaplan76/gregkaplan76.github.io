b6c1d1717957fb8ef3a48e5287cc68936d51fa73
gregkaplan76 <94259458+gregkaplan76@users.noreply.github.com>
2022-08-12 14:30:01 -0700

    p

--- tagged.js
+++ tagged.js
@@ -1,9 +1,8 @@
 (function() {
 
-    var $imgs = $('#gallery img');                  // Store all images
+    var $imgs = $('#gallery .image');                  // Store all images
     var $buttons = $('#buttons');                   // Store buttons element
     var tagged = {};                                // Create tagged object
-  
     $imgs.each(function() {                         // Loop through images and
       var img = this;                               // Store img in variable
       var tags = $(this).data('tags');              // Get this element's tags
